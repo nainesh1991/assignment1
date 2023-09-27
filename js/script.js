6cbd95a83707ffb60da68ac39fb199e8c538f47d
Nainesh <nainesh133@gmail.com>
2023-09-26 20:33:00 -0700

    M2_Assignment

---
+++ js/script.js
@@ -0,0 +1,170 @@
+// STEP 1 Convert the following highlighted identifiers to Camel Case notation:
+
+// let someMonth
+// function theMonth() { }
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2 : Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+
+// let num = 5
+// let name = 'Nainesh'
+// let boolean = true
+// let value = null
+
+// Step 3
+
+// let num1 = 20
+// let num2 = 10
+// let num3 = 40
+// let result
+
+// result = (num1 + num2) * (num3 - num2)
+// console.log(result)
+
+// Step 4
+
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let zipCode
+// let yourAge
+// let strReferralSource
+// let boolMayWeContactYou
+
+// Step 5
+// let firstName, lastName, address;
+
+// firstName = "Nainesh";
+// lastName = "Patel";
+// address = "ABC ave";
+
+// let firstName = "Nainesh";
+// let lastName = "Patel";
+// let address = "ABC ave"
+
+
+// let firstName = 'Nainesh', lastName = 'Patel', address = 'ABC ave'
+
+// Step 6
+
+// let number = 10;
+// let string = '20'
+
+// let coercedResult = number + string;
+
+// console.log(coercedResult)
+
+// Step 7
+
+// let firstBoolean = true
+
+// let gender = 'Male'
+
+// console.log(firstBoolean + gender)
+
+// let secondBoolean = false
+
+// let number = 10
+
+// console.log(secondBoolean + number)
+
+// Step 8
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// Step 9
+
+// let value = null
+// let undefinedValue
+
+// console.log(value)
+// console.log(undefinedValue)
+
+
+// Step 10 Use the unary typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+
+// let strName = typeof ('Nainesh')
+// console.log(strName)
+// let num = typeof (5)
+// console.log(num)
+// let boolean = typeof (false)
+// console.log(boolean)
+// let obj = typeof (object)
+// console.log(obj)
+// console.log(typeof(Object))
+// let x
+// let undefinedvalue = typeof (x)
+// console.log(undefinedvalue)
+
+// Step 11
+
+
+// alert('Hello' + 'Nainesh Patel' + ' ,welcome to the JavaScript class!')
+
+// Step 12
+
+// let name1 = 'Nainesh Patel'
+// alert('Hello' + name1 + ' ,welcome to the JavaScript class!')
+
+// Step 13
+
+// let course = 'JavaScript'
+
+// alert('Hello' + 'Nainesh Patel' + ' ,welcome to the' + course + ' class!')
+
+// Step 14
+// alert('Hello Zak Ruvalcaba.' + '\n Welcome to the JavaScript class!')
+
+// Step 15
+
+// let name1 = prompt('Please enter your name')
+// alert('Hello ' + name1 + ', welcome to the JavaScript class!')
+
+// Step 16
+
+// let course = prompt('Please enter class name that you are taking!')
+// alert(`Hello Nainesh, welcome to the ${course} class!`)
+
+// Step 17
+
+// let x = 10, y = 20
+
+// let sum = x + y
+// console.log(sum)
+
+// Step 18
+
+// let x = 20
+// x += 20
+// console.log(x)
+
+// Step 19
+
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// Step 20
+
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+
+// Step 21
+
+// let x = 10, y = 20
+
+// console.log(x != y && x < y)
+
+// Step 22  Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result within the console window. The application cannot use the same operators used in the previous application.
+
+// let x = 10, y = 20
+
+// console.log(x == y || x >= y)
+
